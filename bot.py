3e5740bd275d516a86f34e8ffb453b7d610b84fe
Kamil Frąckiewicz <hello@olfo.pl>
2026-06-10 09:06:38 +0200

    Fix: BE PRICE_PREC error and dashboard state

--- bot.py
+++ bot.py
@@ -967,7 +967,7 @@ async def pyramid_monitor_loop():
                         be_sl = entry_price * (1 + gstate.be_sl_pct / 100) if ps.pyramid_side == "LONG" else entry_price * (1 - gstate.be_sl_pct / 100)
                         be_triggered = (price >= be_trigger if ps.pyramid_side == "LONG" else price <= be_trigger)
                         if be_triggered:
-                            prec = PRICE_PREC.get(ps.symbol, 4)
+                            prec = {"BTC_USDT":1,"ETH_USDT":2,"SOL_USDT":2,"SUI_USDT":4,"DOGE_USDT":5,"ADA_USDT":4,"LINK_USDT":3,"HYPE_USDT":3,"NAS100_USDT":0,"SP500_USDT":2,"BNB_USDT":1,"XRP_USDT":4,"TRX_USDT":5,"LTC_USDT":2,"AVAX_USDT":3,"ONDO_USDT":4,"UNI_USDT":3,"TAO_USDT":2,"XAU_USDT":2,"ARB_USDT":5,"GALA_USDT":6,"ATOM_USDT":3,"DOT_USDT":3,"ALGO_USDT":4,"JUP_USDT":4,"KAITO_USDT":4,"PENGU_USDT":6,"WLFI_USDT":5,"BCH_USDT":2}.get(ps.symbol, 4)
                             be_sl = round(be_sl, prec)
                             ps.current_sl = be_sl
                             ps.be_activated = True
